eadbcb4efb0caa720df9f43627632ddada2844e2
Nel Greco <nel.greco@student.ucll.be>
2026-01-03 12:38:06 +0100

    Initial commit

--- Invoice_Api/Models/Customer.cs
+++ Invoice_Api/Models/Customer.cs
@@ -7,8 +7,8 @@ namespace Invoice_Api.Models
     {
         public int CustomerId { get; set; }
 
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; } = string.Empty;
 
         public List<Invoice>? Invoices { get; set; }
